1f8d79d1f519a22c54658a1ff276d4fa3877e902
Songysp <hyeon9502@naver.com>
2024-06-24 17:15:10 +0900

    유저 수정

--- src/app.js
+++ src/app.js
@@ -1,34 +1,24 @@
 import express from 'express'
 import morgan from 'morgan'
 import fs from 'fs'
+import cors from 'cors'
 import { connectDB } from "./models/database.js";
 import userRouter from './routers/user.js' //회원정보
 import todolistRouter from './routers/todolist.js' // todolist 
+import mainRouter from './routers/main.js' // main
 import {config}  from './config/config.js';
 
 
 const app = express()
 
-const html = fs.readFileSync('index.html')
+app.use(cors())
+
 app.use(express.json())
 app.use(morgan('combined'))
 
 app.use('/user', userRouter)
 app.use('/list', todolistRouter)
-
-
-app.get('/',(req,res)=>{
-  res.writeHead(200)
-  res.write(html)
-  res.end()
-})
-
-app.get('/index.html',(req,res)=>{
-  res.writeHead(200)
-  res.write(html)
-  res.end()
-})
-
+app.use('/main', mainRouter)
 
 connectDB().then((db) => {
   console.log('몽구스를 사용하여 연결성공')
@@ -36,11 +26,3 @@ connectDB().then((db) => {
     console.log(`서버가 포트 ${config.host.port}에서 실행 중입니다.`);
 });
 }).catch(console.error);
-
-
-
-// "type": "module",
-// npm install express-validator
-// npm install bcrypt
-// npm install jsonwebtoken
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,26 +6,26 @@ import { isAuth } from '../middlewares/user.js';
 
 const router = express.Router()
 
-const validateLogin = [
-    body('userid').trim().notEmpty().withMessage('username을 입력하세요'),
-    body('password').trim().isLength({min: 4}).withMessage('password는 최소 4자 이상 입력하세요'), validate
-];
+// const validateLogin = [
+//     body('userid').trim().notEmpty().withMessage('username을 입력하세요'),
+//     body('password').trim().isLength({min: 4}).withMessage('password는 최소 4자 이상 입력하세요'), validate
+// ];
 
-const validateSignup = [
-    ... validateLogin,
-    body('name').trim().notEmpty().withMessage('name을 입력하세요'),
-    body('email').isEmail().withMessage('이메일 형식 확인하세요'), validate
-];
+// const validateSignup = [
+//     ... validateLogin,
+//     body('name').trim().notEmpty().withMessage('name을 입력하세요'),
+//     body('email').isEmail().withMessage('이메일 형식 확인하세요'), validate
+// ];
 
 
-router.use((req,res,next)=>{
-    console.log('users에 존재하는 미들웨어')
-    next()
-})
+// router.use((req,res,next)=>{
+//     console.log('users에 존재하는 미들웨어')
+//     next()
+// })
 
-router.get('/',(req,res)=>{
-    res.status(200).send('회원정보 확인')
-})
+// router.get('/',(req,res)=>{
+//     res.status(200).send('회원정보 확인')
+// })
 
 
 router.delete('/:userid', isAuth, authController.del_user);
