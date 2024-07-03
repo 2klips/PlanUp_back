cc260521eea6f749720eabba588b35aa30c2b8fa
Songysp <156406181+Songysp@users.noreply.github.com>
2024-07-03 16:11:30 +0900

    채용공고ing

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import userRouter from './user.js';
-import mainRouter from './main.js';
+import recruitmentRouter from './recruitment.js';
 import todolistRouter from './todolist.js';
 
 const router = express.Router();
 
-router.use('/', mainRouter);
-router.use('/main', mainRouter);
+router.use('/recruitment', recruitmentRouter);
 router.use('/user', userRouter);
 router.use('/list', todolistRouter)
 
