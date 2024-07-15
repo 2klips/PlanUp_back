1338b449b761aa0d690feb2ce550479f7ae20f9b
Heeseon06 <seon8315@gmail.com>
2024-07-15 15:26:14 +0900

    main.py수정

--- main.py
+++ main.py
@@ -18,18 +18,12 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://10.0.2.2:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # 모든 도메인 허용
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
 )
 
 class URLRequest(BaseModel):
